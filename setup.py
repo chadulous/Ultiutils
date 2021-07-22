1f61ceaec5ea635a0308a612322035c3dfef964a
maverickquill55 <redrangercj@icloud.com>
2021-07-22 18:00:02 +0000

    auto packaging update 1 half

--- setup.py
+++ setup.py
@@ -1,6 +1,13 @@
 import pathlib
 from setuptools import setup
-
+from datetime import date
+import os
+import requests
+tdate = date.today()
+first = tdate.day
+second = tdate.month
+third = tdate.year
+os.system('rm dist/* && rm build/*')
 # The directory containing this file
 HERE = pathlib.Path(__file__).parent
 
@@ -10,7 +17,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="ultiutils",
-    version="1.0.0",
+    version=f"{first}.{second}.{third}",
     description="utilites module to make coding in python just wayyy faster",
     long_description=README,
     long_description_content_type="text/markdown",
@@ -27,3 +34,8 @@ setup(
     include_package_data=True,
     install_requires=["requests"],
 )
+os.system('twine check dist/* > check.txt')
+check = open('check.txt', 'r').read()
+if check.count('PASSED') == 2:
+  os.system('twine upload dist/*')
+  check.write()
